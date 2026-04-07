274648838fce7d9de091ca390dff0f610ceea3ef
Teja G <gteja6877@gmail.com>
2026-04-07 09:00:46 +0530

    Add files via upload

---
+++ citizen.py
@@ -0,0 +1,7 @@
+name = input("Enter citizen name:")
+Dob = (int(input("Enter dob (DD/MM/YYYY):")))
+age=2026-Dob
+if age>= 60:
+    print("Senior Citizen ")
+    else:
+         print("Not a senior citizen")
