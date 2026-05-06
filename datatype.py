19f9c9dea3596cf2a07c4c7b9a9ce5cbe202f9da
Teja G <gteja6877@gmail.com>
2026-05-06 10:05:03 +0530

    Add files via upload
    a is Type of <class 'int'>
    b is Type of <class 'float'>
    c is Type of <class 'complex'>

---
+++ datatype.py
@@ -0,0 +1,6 @@
+a=10; 
+b=11.5; 
+c=2.05j; 
+print("a is Type of",type(a)); #prints type of variable a
+print("b is Type of",type(b)); #prints type of variable b
+print("c is Type of",type(c)); #prints type of variable c
